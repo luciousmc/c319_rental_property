4044dd784fc13fc91e38ea6e587b1ab970bbd367
Marlon Clay <luciousmc@icloud.com>
2019-06-10 19:41:58 +0000

    removed tenant_id & added date formate

--- server/public/api/unit/add_tenant.php
+++ server/public/api/unit/add_tenant.php
@@ -5,7 +5,6 @@ startup();
 $input = file_get_contents('php://input');
 $_POST = json_decode($input, true);
 
-// $tenantID = $_POST['tenant_id'];
 $unitID = $_POST['unit_id'];
 $businessName = $_POST['business_name'];
 $contactName = $_POST['contact_name'];
@@ -14,14 +13,14 @@ $tenantEmail = $_POST['tenant_email'];
 $moveInDate = $_POST['move_in_date'];
 $leaseEndDate = $_POST['lease_end_date'];
 $rentDueDate = $_POST['rent_due_date'];
-$rent = $_POST['rent'];
 
-$query = "INSERT INTO `tenants` (`tenant_id`, `unit_id`, `business_name`, `contact_name`, `tenant_phone`, `tenant_email`, `move_in_date`, `lease_end_date`, `rent_due_date`, `rent`)
-VALUES ('$unitID', '$businessName', '$contactName', '$tenantPhone', '$tenantEmail', '$moveInDate', '$leaseEndDate', '$rentDueDate', '$rent')";
+$query = "INSERT INTO `tenants` 
+(`unit_id`, `business_name`, `contact_name`, `tenant_phone`, `tenant_email`, `move_in_date`, `lease_end_date`, `rent_due_date`) VALUES 
+(1, '$businessName', '$contactName', '$tenantPhone', '$tenantEmail', DATE_FORMAT('2019-04-05', '%Y-%m-%d'), DATE_FORMAT('2019-09-21', '%Y-%m-%d'), DATE_FORMAT('2010-01-05', '%Y-%m-%d'))";
 // print($query);
 $result = mysqli_query($conn, $query);
 if(!$result){
-    print(mysqli_error($conn));
+    throw new Exception(mysqli_error($conn));
     exit();
 }
 print(json_encode(
